3b355d85b1d55dc5b2dcc6111c987773020420f5
Kyle <kvonderw@uci.edu>
2018-10-09 23:26:28 -0700

    .

--- models/issuesearch.py
+++ models/issuesearch.py
@@ -11,25 +11,30 @@ Example issues search: https://github.com/search?
 
 BASE_URL = 'https://github.com/search?'
 
-class URL(Base):
+class IssueSearch(Base):
 
     language = ['Language(s) to be searched for.']
-    is_archived = 'Flag to included archived issues. Default false; to not return archived issues.'
-    is_open = 'Flag to included open or closed issues. Dafault True; to return open issues'
-    users = ['Includ issues created for projects belonging to some user(s).']
+    is_archived = 'Flag to include archived issues. Default false; to not return archived issues.'
+    is_open = 'Flag to include open or closed issues. Default True; to return open issues'
+    users = ['Include issues created for projects belonging to some user(s).']
+    orgs = ['Include issues created for projects belonging to some org(s) or org group.']
     labels = ['Include issues with some label(s).']
     page = 'Tracks current results page.'
+    type = 'Choose pr or issue or pr and issue. Default to show both pr and issues.'
 
-    def __init__(self, languages=[], users=[], labels=[], is_open=True, is_archived=False, page=1):
+    def __init__(self, languages=[], users=[], orgs=[],labels=[], is_open=True, is_archived=False, page=1, type='issueandpr'):
         self.languages = languages
         self.users = users
+        self.orgs = orgs
         self.labels = labels
         self.is_archived = is_archived
         self.is_open = is_open
         self.page = page
+        self.type = type
         super(Base, self).__init__()
 
     def build_url(self):
         pass
 
-print(URL().help())
+
+print(IssueSearch().help())
